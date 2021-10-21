7e06245f9f7ee81c4060dbf78c69b676d82cc46a
RiscadoA <RiscadoA@users.noreply.github.com>
2021-10-21 14:32:15 +0000

    deploy: 37aad1e1b2798bf6cd07d7466e17cea63115714b

--- search/functions_5.js
+++ search/functions_5.js
@@ -3,5 +3,5 @@ var searchData=
   ['readcamera_42',['readCamera',['../classhs_1_1robot_1_1HSRobot.html#ac4189df941df1a9d17c7d04c930083f2',1,'hs::robot::HSRobot::readCamera()'],['../classhs_1_1robot_1_1Robot.html#a05b532c630d7988a99c5f2999d2c10e9',1,'hs::robot::Robot::readCamera()'],['../classhs_1_1robot_1_1WebotsRobot.html#a3de90e9222d6fe998965c65e3f47eb28',1,'hs::robot::WebotsRobot::readCamera()']]],
   ['readsensor_43',['readSensor',['../classhs_1_1robot_1_1HSRobot.html#a45f173e55e6d08101de1c2c1278cde21',1,'hs::robot::HSRobot::readSensor()'],['../classhs_1_1robot_1_1Robot.html#a93b5789cae330123ace1023443b24d4f',1,'hs::robot::Robot::readSensor()'],['../classhs_1_1robot_1_1WebotsRobot.html#ae46c7a8800bc2f10a5b9b101e2be4287',1,'hs::robot::WebotsRobot::readSensor()']]],
   ['registertype_44',['registerType',['../classhs_1_1behaviour_1_1Manager.html#a62227a4235cb200c06d64b32c983e874',1,'hs::behaviour::Manager']]],
-  ['reset_45',['reset',['../classhs_1_1behaviour_1_1Behaviour.html#a12fd1fb8fe4b7571a548ee1e81ce6a4d',1,'hs::behaviour::Behaviour']]]
+  ['reset_45',['reset',['../classhs_1_1behaviour_1_1Behaviour.html#a12fd1fb8fe4b7571a548ee1e81ce6a4d',1,'hs::behaviour::Behaviour::reset()'],['../classhs_1_1behaviour_1_1Manager.html#a8cb433965677ad622ee43d59e24262e7',1,'hs::behaviour::Manager::reset()']]]
 ];
