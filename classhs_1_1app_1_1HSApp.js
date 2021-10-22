f355c43bec2e153cd912849a2813907502f16632
RiscadoA <RiscadoA@users.noreply.github.com>
2021-10-22 11:07:42 +0000

    deploy: 5b5d62179c343e4bbd002e052732a1d2a3246146

--- classhs_1_1app_1_1HSApp.js
+++ classhs_1_1app_1_1HSApp.js
@@ -1,7 +1,7 @@
 var classhs_1_1app_1_1HSApp =
 [
     [ "HSApp", "classhs_1_1app_1_1HSApp.html#abb182b63b34742856250034227292e88", null ],
-    [ "~HSApp", "classhs_1_1app_1_1HSApp.html#a5da7309374fb7224d62c7337e6e696bb", null ],
+    [ "~HSApp", "classhs_1_1app_1_1HSApp.html#aa41079b6a62fbe293b29a9f46a1932c5", null ],
     [ "getCameraAngle", "classhs_1_1app_1_1HSApp.html#a9dfada2ad0b55bca0afcb079607b0202", null ],
     [ "getJoystickX", "classhs_1_1app_1_1HSApp.html#a0cc18ee6b68cc5b78794d2b2b82a816d", null ],
     [ "getJoystickY", "classhs_1_1app_1_1HSApp.html#a441a9c8dc1e0a33bd8f9e2de35dad0b7", null ],
